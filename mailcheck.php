0c05b28541972ed50cd31212f4e38bd9f7b4038e
Matthew Sigley <matthew.d.sigley@gmail.com>
2022-09-30 16:16:54 -0400

    1.3
    Removed Sift4 text diff algorithm and replaced it with PHP's built in levenshtein algorithm. Sift4 was too greedy and would return suggestions unrelated to the users input.
    Added in memory cache of DNS domain validation results. This improves performance when validating multiple emails in a row.
    Added character transposition handling to improve suggestions since levenshtein doesn't account for transpositions.
    Added "off by one character" handling to improve suggestions where the user is missing the last character of the domain.
    Added second level domain overrides to improve gmail suggestions.

--- mailcheck.php
+++ mailcheck.php
@@ -8,7 +8,7 @@
  * -DNS based email validation
  *
  * @author Matthew Sigley, based on Mailcheck.js by Derrick Ko
- * @version 1.2
+ * @version 1.3
  * @license GNU GPL version 3 (or later)
  **/
 
@@ -17,6 +17,8 @@
 	private $allowed_domain_chars;
 
 	private $settings;
+
+	private $dns_cache = array();
 
 	private $defaults = array(
 		'domain_threshold' => 2,
@@ -29,11 +31,17 @@
 			'gmail.com', 'ymail.com', 'aim.com', 'rogers.com', 'verizon.net', 'rocketmail.com', 
 			'google.com', 'optonline.net', 'sbcglobal.net', 'aol.com', 'me.com', 'btinternet.com',
 			'charter.net', 'shaw.ca', 'apple.com', 'google.com', 'hotmail.com', 'yahoo.com',
+			'outlook.com', 'googlemail.com',
 			// Top 20 Mail.com domains https://www.mail.com/email/#.7518-header-subnav1-4
 			'mail.com', 'email.com', 'usa.com', 'myself.com', 'consultant.com', 'post.com',
 			'europe.com', 'asia.com', 'iname.com', 'writeme.com', 'dr.com', 'cheerful.com', 
 			'accountant.com', 'techie.com', 'engineer.com', 'linuxmail.org', 'musician.org',
 			'contractor.net', 'financier.com', 'workmail.com'
+		),
+		'second_level_domains_overrides' => array( 
+			// Gmail is always gmail.com
+			// https://www.gmass.co/blog/domains-gmail-com-googlemail-com-and-google-com/
+			'gmail' => 'gmail.com', 'googlemail' => 'googlemail.com'
 		),
 		'second_level_domains' => array( 'yahoo', 'hotmail', 'mail', 'live', 'outlook', 'gmx' ),
 		'top_level_domains' => array( 
@@ -66,7 +74,7 @@
 	}
   
 	// Returns suggested email
-	function suggest( $original_email ) {
+	public function suggest( $original_email ) {
 		$email_parts = $this->parse_email( $original_email );
 
 		// Can't make suggestions if no domain
@@ -78,13 +86,28 @@
 		$valid_tld = isset( $this->settings->top_level_domains[ $email_parts->tld ] );
 
 		// If email has a valid domain
-		if( $valid_domain || $valid_sld && $valid_tld ) {
+		$valid = false;
+		if( $valid_domain ) {
+			$valid = true;
+		} elseif( isset( $this->settings->second_level_domains_overrides[$email_parts->sld] ) ) {
+			$valid = true;
+			$email_parts->domain = $this->settings->second_level_domains_overrides[$email_parts->sld];
+		} elseif( $domain = $this->find_one_character_off( $email_parts->domain, $this->settings->domains ) ) {
+			$valid = true;
+			$email_parts->domain = $domain;
+		} elseif( $valid_sld && $valid_tld ) {
+			$valid = true;
+		}
+
+		if( $valid ) {
 			// Return the sanitized email if its different than the original
 			$email = $email_parts->mailbox . '@' . $email_parts->domain;
 			if( $email == $original_email )
 				return false;
 			return $email;
 		}
+
+
 		
 		$domain = $email_parts->domain;
 		$sld = $email_parts->sld;
@@ -138,7 +161,7 @@
 
 	// Parses email into its parts.
 	// Sanitizes each individual part of the email.
-	function parse_email( $email ) {
+	public function parse_email( $email ) {
 		$email_parts = new StdClass();
 		$email_parts->mailbox = '';
 		$email_parts->domain = '';
@@ -166,7 +189,7 @@
 	}
 
 	// Validation Functions
-	function validate_email( $email, $validate_dns = false ) {
+	public function validate_email( $email, $validate_dns = false ) {
 		$valid = (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
 		if( !$valid )
 			return false;
@@ -179,7 +202,11 @@
 		// full-stop trailing char so that the default domain of the server
 		// is not added automatically
 		$dns_failed = false;
+
 		list( $mailbox, $domain ) = explode( '@', $email, 2);
+
+		if( isset( $this->dns_cache[$domain] ) )
+			return $this->dns_cache[$domain];
 
 		set_error_handler(array($this, 'throwException'));
 		try {
@@ -200,23 +227,35 @@
 				$dns_record_types = array_map( function($element) { return $element['type']; }, $dns_record );
 			// Below could be multiple ands but its easier to read with if's
 			if( !in_array('MX', $dns_record_types) ) { 
-				if( in_array('A', $dns_record_types) || in_array('AAAA', $dns_record_types) )
+				$email_parts = $this->parse_email( $email );
+
+				if( isset( $this->settings->second_level_domains_overrides[$email_parts->sld] ) 
+					|| $this->find_one_character_off( $email_parts->domain, $this->settings->domains ) ) {
+					$this->dns_cache[$domain] = false;
+					return false;
+				}
+
+				if( in_array('A', $dns_record_types) || in_array('AAAA', $dns_record_types) ) {
 					// Check for NS record for a valid domain
 					// Bad DNS providers return A records for NXDOMAIN errors
-					if( !in_array('NS', $dns_record_types) )
+					if( !in_array('NS', $dns_record_types) ) {
+						$this->dns_cache[$domain] = false;
 						return false;
+					}
+				}
 			}
 		}
 
+		$this->dns_cache[$domain] = true;
 		return true;
 	}
 
 	// Sanitation Functions
-	function sanitize_email( $email ) {
+	public function sanitize_email( $email ) {
 		return filter_var($email, FILTER_SANITIZE_EMAIL);
 	}
 
-	function sanitize_mailbox( $mailbox ) {
+	public function sanitize_mailbox( $mailbox ) {
 		$mailbox = str_split( $mailbox );
 		foreach( $mailbox as &$char ) {
 			if( !isset( $this->allowed_mailbox_chars[ $char ] ) )
@@ -228,8 +267,8 @@
 		//return preg_replace( '/[^a-z0-9!#$%&\'*+\/=?^_`{|}~\.-]+/i', '', $domain );
 	}
 
-	function sanitize_domain( $domain ) {
-		$domain = str_split( $domain );
+	public function sanitize_domain( $domain ) {
+		$domain = str_split( strtolower( $domain ) );
 		foreach( $domain as &$char ) {
 			if( !isset( $this->allowed_domain_chars[ $char ] ) )
 				$char = '';
@@ -241,16 +280,15 @@
 	}
 
 	// String comparision functions
-	function find_closest_domain( $domain, $has_dot = true ) {
+	public function find_closest_domain( $domain, $has_dot = true ) {
 		$distance = false;
 		$min_distance = 99;
 		$closest = false;
 		$threshold = $has_dot ? $this->settings->domain_threshold : $this->settings->second_level_threshold;
-		$max_threshold = floor( strlen( $domain ) / 2 );
+		$max_threshold = (int) floor( strlen( $domain ) / 2 );
 
 		if( $threshold > $max_threshold )
 			$threshold = $max_threshold;
-		//var_dump( $threshold );
 
 		foreach ( $this->settings->domains as $canon ) {
 			if( $domain === $canon )
@@ -258,35 +296,38 @@
 			
 			// If tld is missing, only compare slds
 			if( $has_dot )
-				$distance = $this->sift4( $domain, $canon );
+				$distance = levenshtein( $domain, $canon );
 			else
-				$distance = $this->sift4( $domain, substr( $canon, 0, strpos( $canon, '.' ) ) );
+				$distance = levenshtein( $domain, substr( $canon, 0, strpos( $canon, '.' ) ) );
 
+			if( $domain == 'yahoo.com' )
+				var_dump( $distance );
+			
 			if( $distance < $min_distance ) {
 				$min_distance = $distance;
 				$closest = $canon;
 			}
 		}
-		
+
 		if( $min_distance <= $threshold && !empty( $closest ) )
 			return $closest;
 		
 		return false;
 	}
 
-	function find_closest_sld( $sld ) {
+	public function find_closest_sld( $sld ) {
 		return $this->find_closest( $sld, $this->settings->second_level_domains, $this->settings->second_level_threshold );
 	}
 
-	function find_closest_tld( $tld ) {
+	public function find_closest_tld( $tld ) {
 		return $this->find_closest( $tld, $this->settings->top_level_domains, $this->settings->top_level_threshold );
 	}
 
-	function find_closest( $needle, $haystack, $threshold ) {
+	public function find_closest( $needle, $haystack, $threshold ) {
 		$distance = false;
 		$min_distance = 99;
 		$closest = false;
-		$max_threshold = floor( strlen( $needle ) / 2 );
+		$max_threshold = (int) floor( strlen( $needle ) / 2 );
 
 		if( $threshold > $max_threshold )
 			$threshold = $max_threshold;
@@ -295,7 +336,11 @@
 			if( $needle === $canon )
 				return $needle;
 			
-			$distance = $this->sift4( $needle, $canon );
+			$num_transpositions = $this->num_transpositions( $needle, $canon );
+			if( $num_transpositions > 0 && $num_transpositions <= $threshold ) 
+				return $canon;
+
+			$distance = levenshtein( $needle, $canon );
 
 			if( $distance < $min_distance ) {
 				$min_distance = $distance;
@@ -309,103 +354,50 @@
 		return false;
 	}
 
-	// Sift4 - common version
-	// online algorithm to compute the distance between two strings in O(n)
-	// $maxOffset is the number of characters to search for matching letters
-	// $maxDistance is the distance at which the algorithm should stop computing the value and just exit (the strings are too different anyway)
-	// Props to Ferenc Szatmári via https://siderite.blogspot.com/2014/11/super-fast-and-accurate-string-distance.html#at713425542
-	function sift4( $s1, $s2, $maxOffset = 5, $maxDistance = 0 ) {
-		if (!$s1 || !strlen($s1)) {
-				if (!$s2) {
-						return 0;
-				}
-				return strlen($s2);
+	public function find_one_character_off( $needle, $haystack ) {
+		$length = strlen( $needle );
+		foreach ( $haystack as $canon ) {
+			if( $length + 1 === strlen( $canon ) && $needle === substr( $canon, 0, $length ) )
+				return $canon;
 		}
 
-		if (!$s2 || !strlen($s2)) {
-				return strlen($s1);
-		}
-
-		$l1 = strlen($s1);
-		$l2 = strlen($s2);
-
-		$c1 = 0; //cursor for string 1
-		$c2 = 0; //cursor for string 2
-		$lcss = 0; //largest common subsequence
-		$local_cs = 0; //local common substring
-		$trans = 0; //number of transpositions ('ab' vs 'ba')
-		$offset_arr = array(); //offset pair array, for computing the transpositions
-		while (($c1 < $l1) && ($c2 < $l2)) {
-				if (substr($s1, $c1, 1) == substr($s2, $c2, 1)) {
-						$local_cs++;
-						$isTrans = false;
-						$i = 0;
-						while ($i < sizeof($offset_arr)) { //see if current match is a transposition
-								$ofs = $offset_arr[$i];
-								if ($c1 <= $ofs['c1'] || $c2 <= $ofs['c2']) {
-										$isTrans = abs($c2 - $c1) >= abs($ofs['c2'] - $ofs['c1']);
-										if ($isTrans) {
-												$trans++;
-										} else {
-												if (!$ofs['trans']) {
-														$ofs['trans'] = true;
-														$trans++;
-												}
-										}
-										break;
-								} else {
-										if ($c1 > $ofs['c2'] && $c2 > $ofs['c1']) {
-												array_splice($offset_arr, $i, 1);
-										} else {
-												$i++;
-										}
-								}
-						}
-						array_push($offset_arr, array('c1' => $c1, 'c2' => $c2, 'trans' => $isTrans));
-				} else {
-						$lcss += $local_cs;
-						$local_cs = 0;
-						if ($c1 != $c2) {
-								$c1 = $c2 = min($c1, $c2); //using min allows the computation of transpositions
-						}
-						//if matching characters are found, remove 1 from both cursors (they get incremented at the end of the loop)
-						//so that we can have only one code block handling matches
-						for ($i = 0; $i < $maxOffset && ($c1 + $i < $l1 || $c2 + $i < $l2); $i++) {
-								if (($c1 + $i < $l1) && (substr($s1, $c1 + $i, 1) == substr($s2, $c2, 1))) {
-										$c1 += $i - 1;
-										$c2--;
-										break;
-								}
-								if (($c2 + $i < $l2) && (substr($s1, $c1, 1) == substr($s2, $c2 + $i, 1))) {
-										$c1--;
-										$c2 += $i - 1;
-										break;
-								}
-						}
-				}
-				$c1++;
-				$c2++;
-				if ($maxDistance) {
-						$temporaryDistance = max($c1, $c2) - $lcss + $trans;
-						if ($temporaryDistance >= $maxDistance)
-								return $temporaryDistance;
-				}
-				// this covers the case where the last match is on the last token in list, so that it can compute transpositions correctly
-				if (($c1 >= $l1) || ($c2 >= $l2)) {
-						$lcss += $local_cs;
-						$local_cs = 0;
-						$c1 = $c2 = min($c1, $c2);
-				}
-		}
-		$lcss += $local_cs;
-		return max($l1, $l2) - $lcss + $trans; //apply transposition cost to final result
+		return false;
 	}
 
-	function throwException($severity, $message, $file, $line) {
+	public function num_transpositions( $actual, $desired ) {
+		if( $desired === $actual )
+			return 0;
+	
+		$dlength = strlen( $desired );
+		if( $dlength !== strlen( $actual ) )
+			return -1;
+		
+		$array1 = str_split( $desired );
+		$array2 = str_split( $actual );
+
+		$transpositions = 0;		
+		for( $i = 0; $i<$dlength; $i++ ) {
+			if( $array1[$i] === $array2[$i] )
+				continue;
+
+			if( $i+1 < $dlength && $array1[$i] === $array2[$i+1] && $array1[$i+1] === $array2[$i] ) {
+				// Transpose characters
+				$array2[$i] = $array1[$i];
+				$array2[$i+1] = $array1[$i+1];
+				$transpositions++;
+			} else {
+				return -1; // String is out of order more than flipped characters
+			}
+		}
+
+		return $transpositions;
+	}
+
+	private function throwException($severity, $message, $file, $line) {
 		if (!(error_reporting() & $severity)) {
 			// This error code is not included in error_reporting
 			return;
 		}
-        throw new ErrorException($message, 0, $severity, $file, $line);
+		throw new ErrorException($message, 0, $severity, $file, $line);
   }
- }
+}
